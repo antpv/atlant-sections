dcd8e7619b4d1580bf0f705ffe4791c251c3731e
Antipov <antpv99@gmail.com>
2020-12-15 10:47:16 +0200

    Initial commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import './assets/scss/_main.scss'
 
 Vue.config.productionTip = false
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+const application = new Vue({
+  render: h => h(App)
+})
+
+application.$mount('#app')
